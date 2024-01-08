ad687e8997232b32181ce7065ad8c327db1c5ced
Dimarik <dzhikon@bk.ru>
2024-01-09 01:44:18 +0300

    change README.md

---
+++ dirty_main.py
@@ -0,0 +1,6 @@
+from accounting_pachege.salary import *
+from accounting_pachege.people import *
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
---
+++ main.py
@@ -0,0 +1,9 @@
+from accounting_pachege.salary import calculate_salary
+from accounting_pachege.people import get_employees
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.now())
